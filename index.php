00aec971701814be0946eb0360b0eb8bca2378a1
Guillermo Quiñones Villarreal <guillermo@lagrancorporacion.com>
2014-07-15 08:57:50 -0500

    inicio
    inicio

---
+++ index.php
@@ -0,0 +1,111 @@
+﻿<!DOCTYPE html>
+
+<html lang="es">
+<head>
+	<meta charset="utf-8" />
+	<title>Proyecto memo market</title>
+	<meta name="descripcion" content="Bazar Online de Automoviles" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link type="text/css" rel="stylesheet" href="recursos/css/normalize.css" />
+	<link rel="stylesheet" href="recursos/foundation/css/foundation.css" />
+	<script type="text/javascript" src="recursos/js/jquery.js" ></script>
+    <script src="recursos/foundation/js/vendor/modernizr.js"></script>
+</head>
+<body>
+	<header>
+	</header>
+	
+	<section>
+		<form>
+		  <div class="row">
+			<div class="large-12 columns">
+			  <label>Input Label
+				<input type="text" placeholder="large-12.columns" />
+			  </label>
+			</div>
+		  </div>
+		  <div class="row">
+			<div class="large-4 columns">
+			  <label class="error">Input Label
+				<input type="text" class="error" placeholder="large-4.columns" />
+			  </label>
+			  <small class="error">Invalid entry</small>
+			</div>
+			<div class="large-4 columns">
+			  <label>Input Label
+				<input type="text" placeholder="large-4.columns" />
+			  </label>
+			</div>
+			<div class="large-4 columns">
+			  <div class="row collapse">
+				<label>Input Label</label>
+				<div class="small-9 columns">
+				  <input type="text" placeholder="small-9.columns" />
+				</div>
+				<div class="small-3 columns">
+				  <span class="postfix">.com</span>
+				</div>
+			  </div>
+			</div>
+		  </div>
+		  <div class="row">
+			<div class="large-12 columns">
+			  <label>Select Box
+				<select>
+				  <option value="husker">Husker</option>
+				  <option value="starbuck">Starbuck</option>
+				  <option value="hotdog">Hot Dog</option>
+				  <option value="apollo">Apollo</option>
+				</select>
+			  </label>
+			</div>
+		  </div>
+		  <div class="row">
+			<div class="large-6 columns">
+			  <label>Choose Your Favorite</label>
+			  <input type="radio" name="pokemon" value="Red" id="pokemonRed"><label for="pokemonRed">Red</label>
+			  <input type="radio" name="pokemon" value="Blue" id="pokemonBlue"><label for="pokemonBlue">Blue</label>
+			</div>
+			<div class="large-6 columns">
+			  <label>Check these out</label>
+			  <input id="checkbox1" type="checkbox"><label for="checkbox1">Checkbox 1</label>
+			  <input id="checkbox2" type="checkbox"><label for="checkbox2">Checkbox 2</label>
+			</div>
+		  </div>
+		  <div class="row">
+			<div class="large-12 columns">
+			  <label>Textarea Label
+				<textarea placeholder="small-12.columns"></textarea>
+			  </label>
+			</div>
+		  </div>
+		</form>
+		
+		<form>
+		  <div class="row">
+			<div class="large-6 columns">
+			  <label class="error">Error
+				<input type="text" class="error" placeholder="guillermo" />
+			  </label>
+			  <small class="error">Invalid entry</small>
+			</div>
+			<div class="large-6 columns error">
+			  <label>Another Error
+				<input type="text" />
+			  </label>
+			  <small class="error">Invalid entry</small>
+			</div>
+		  </div>
+		  <div class="large-12 columns">
+			  <textarea class="error" placeholder="Message..."></textarea>
+			  <small class="error">Invalid entry</small>
+		  </div>
+		</form>
+		
+	</section>
+	
+	<footer>
+	</footer>
+</body>
+	
+</html>
---
+++ ui.php
@@ -0,0 +1,286 @@
+<!DOCTYPE html>
+
+<html lang="es">
+<head>
+	<meta charset="utf-8" />
+	<title>Proyecto memo market</title>
+	<meta name="descripcion" content="Bazar Online de Automoviles" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link type="text/css" rel="stylesheet" href="recursos/css/normalize.css" />
+	<link rel="stylesheet" href="recursos/foundation/css/foundation.css" />
+	<script type="text/javascript" src="recursos/js/jquery.js" ></script>
+    <script src="recursos/foundation/js/foundation.min.js"></script>
+	<script type="text/javascript">
+		 $(window).bind("load", function () {
+			$(document).foundation();
+		});
+	</script>
+</head>
+<body>
+	<div class="row" style="border:1px solid black">
+		<div class="large-2 columns">
+			pallet #1
+		</div>
+		<div class="large-2 columns" style="background:#1B676B">
+			  <p>#1B676B</p>
+		</div>
+		<div class="large-2 columns" style="background:#519548">
+			  <p>#519548</p>
+		</div>
+		<div class="large-2 columns" style="background:#88C425">
+			  <p>#88C425</p>
+		</div>
+		<div class="large-2 columns" style="background:#BEF202">
+			  <p>#BEF202</p>
+		</div>
+		<div class="large-2 columns" style="background:#EAFDE6">
+			  <p>#EAFDE6</p>
+		</div>
+	</div>
+	<br />
+	<div class="row" style="border:1px solid black">
+		<div class="large-2 columns">
+			pallet #2
+		</div>
+		<div class="large-2 columns" style="background:#30261C">
+			  <p>#30261C</p>
+		</div>
+		<div class="large-2 columns" style="background:#403831">
+			  <p>#403831</p>
+		</div>
+		<div class="large-2 columns" style="background:#36544F">
+			  <p>#36544F</p>
+		</div>
+		<div class="large-2 columns" style="background:#1F5F61">
+			  <p>#1F5F61</p>
+		</div>
+		<div class="large-2 columns" style="background:#0B8185">
+			  <p>#0B8185</p>
+		</div>
+	</div>
+	<br />
+	<div class="row" style="border:1px solid black">
+		<div class="large-2 columns">
+			pallet #3
+		</div>
+		<div class="large-2 columns" style="background:#046D8B">
+			  <p>#046D8B</p>
+		</div>
+		<div class="large-2 columns" style="background:#309292">
+			  <p>#309292</p>
+		</div>
+		<div class="large-2 columns" style="background:#2FB8AC">
+			  <p>#2FB8AC</p>
+		</div>
+		<div class="large-2 columns" style="background:#93A42A">
+			  <p>#93A42A</p>
+		</div>
+		<div class="large-2 columns" style="background:#ECBE13">
+			  <p>#ECBE13</p>
+		</div>
+	</div>
+	<br />
+	<div class="row" style="border:1px solid black">
+		<div class="large-2 columns">
+			pallet #4
+		</div>
+		<div class="large-2 columns" style="background:#FFFF00">
+			  <p>#FFFF00</p>
+		</div>
+		<div class="large-2 columns" style="background:#CCD91A">
+			  <p>#CCD91A</p>
+		</div>
+		<div class="large-2 columns" style="background:#99B333">
+			  <p>#99B333</p>
+		</div>
+		<div class="large-2 columns" style="background:#668C4D">
+			  <p>#668C4D</p>
+		</div>
+		<div class="large-2 columns" style="background:#336666">
+			  <p>#336666</p>
+		</div>
+	</div>
+	<br />
+	<div class="row" style="border:1px solid black">
+		<div class="large-2 columns">
+			pallet #5
+		</div>
+		<div class="large-2 columns" style="background:#FB6900">
+			  <p>#FB6900</p>
+		</div>
+		<div class="large-2 columns" style="background:#F63700">
+			  <p>#F63700</p>
+		</div>
+		<div class="large-2 columns" style="background:#004853">
+			  <p>#004853</p>
+		</div>
+		<div class="large-2 columns" style="background:#007E80">
+			  <p>#007E80</p>
+		</div>
+		<div class="large-2 columns" style="background:#00B9BD">
+			  <p>#00B9BD</p>
+		</div>
+	</div>
+	<br />
+	<div class="row">
+	  <div class="small-2 large-4 columns" style="border:1px solid black">small-2 large-4 small-centered columns</div>
+	  <div class="small-4 large-4 columns" style="border:1px solid black">small-4 large-4 columns</div>
+	  <div class="small-6 large-4 columns" style="border:1px solid black">small-6 large-4 columns</div>
+	</div>
+	<div class="row">
+	  <div class="large-3 columns" style="border:1px solid black">large-3 columns</div>
+	  <div class="large-6 columns" style="border:1px solid black">large-6 columns</div>
+	  <div class="large-3 columns" style="border:1px solid black">large-3 columns</div>
+	</div>
+	<div class="row">
+	  <div class="small-6 large-2 columns" style="border:1px solid black">small-6 large-2 columns</div>
+	  <div class="small-6 large-8 columns" style="border:1px solid black">small-6 large-8 columns</div>
+	  <div class="small-12 large-2 columns" style="border:1px solid black">small-12 large-2 columns</div>
+	</div>
+	<div class="row">
+	  <div class="small-3 columns" style="border:1px solid black">small-3 columns</div>
+	  <div class="small-9 columns" style="border:1px solid black">small-9 columns</div>
+	</div>
+	<div class="row">
+	  <div class="large-4 columns" style="border:1px solid black">large-4 columns</div>
+	  <div class="large-8 columns" style="border:1px solid black">large-8 columns</div>
+	</div>
+	<div class="row">
+	  <div class="small-6 large-5 columns" style="border:1px solid black">small-6 large-5 columns</div>
+	  <div class="small-6 large-7 columns" style="border:1px solid black">small-6 large-7 columns</div>
+	</div>
+	<div class="row">
+	  <div class="large-6 columns" style="border:1px solid black">large-6 columns</div>
+	  <div class="large-6 columns" style="border:1px solid black">large-6 columns</div>
+	</div>
+	<br />
+	<ul class="small-block-grid-4">
+	  <li style="border:1px solid black">hola mundo</li>
+	  <li style="border:1px solid black">que onda?</li>
+	  <li style="border:1px solid black">wasa!!</li>
+	  <li style="border:1px solid black">hola mundo</li>
+	  <li style="border:1px solid black">que onda?</li>
+	  <li style="border:1px solid black">wasa!!</li>
+	</ul>
+	<br />
+	<div class="alert-box [radius round] small-2" data-alert>Alert<a href="#" class="close">&times;</a></div>
+	<div class="progress [radius round] small-8 small-centered">
+	  <span class="meter" style="width:30%;">30%</span>
+	</div>
+	<a href="#" class="button [radius round]">Button</a>
+	<span class="label [radius round]">Label</span>
+	<span data-tooltip class="[radius round]" title="Tooltip">Tooltip</span>
+	
+	<br />
+	<div class="off-canvas-wrap" data-offcanvas>
+  <div class="inner-wrap">
+
+    <a class="left-off-canvas-toggle" href="#" >Menu</a>
+
+    <!-- Off Canvas Menu -->
+    <aside class="left-off-canvas-menu">
+        <!-- whatever you want goes here -->
+        <ul>
+          <li><a href="#">Item 1</a></li>
+        ...
+        </ul>
+    </aside>
+
+    <!-- main content goes here -->
+    <p>Set in the year 0 F.E. ("Foundation Era"), The Psychohistorians opens on Trantor, the capital of the 12,000-year-old Galactic Empire. Though the empire appears stable and powerful, it is slowly decaying in ways that parallel the decline of the Western Roman Empire. Hari Seldon, a mathematician and psychologist, has developed psychohistory, a new field of science and psychology that equates all possibilities in large societies to mathematics, allowing for the prediction of future events.</p>
+
+  <!-- close the off-canvas menu -->
+  <a class="exit-off-canvas"></a>
+
+  </div>
+</div>
+<br/>
+<div class="row">
+	<div class="small-12 column">
+        <ul class="example-orbit-content" data-orbit>
+          <li data-orbit-slide="headline-1">
+            <div>
+              <h2>Headline 1</h2>
+              <h3>Subheadline</h3>
+            </div>
+          </li>
+          <li data-orbit-slide="headline-2">
+            <div>
+              <h2>Headline 2</h2>
+              <h3>Subheadline</h3>
+            </div>
+          </li>
+          <li data-orbit-slide="headline-3">
+            <div>
+              <h2>Headline 3</h2>
+              <h3>Subheadline</h3>
+            </div>
+          </li>
+        </ul>
+        <form>
+      <div class="row">
+        <div class="large-12 columns">
+          <label class="error">Input Label
+            <input type="text" placeholder="large-12.columns" />
+          </label>
+          <small class="error">Invalid entry</small>
+        </div>
+      </div>
+      <div class="row">
+        <div class="large-4 columns">
+          <label>Input Label
+            <input type="text" placeholder="large-4.columns" />
+          </label>
+        </div>
+        <div class="large-4 columns">
+          <label>Input Label
+            <input type="text" placeholder="large-4.columns" />
+          </label>
+        </div>
+        <div class="large-4 columns">
+          <div class="row collapse">
+            <label>Input Label</label>
+            <div class="small-9 columns">
+              <input type="text" placeholder="small-9.columns" />
+            </div>
+            <div class="small-3 columns">
+              <span class="postfix">.com</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="large-12 columns">
+          <label>Select Box
+            <select>
+              <option value="husker">Husker</option>
+              <option value="starbuck">Starbuck</option>
+              <option value="hotdog">Hot Dog</option>
+              <option value="apollo">Apollo</option>
+            </select>
+          </label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="large-6 columns">
+          <label>Choose Your Favorite</label>
+          <input type="radio" name="pokemon" value="Red" id="pokemonRed"><label for="pokemonRed">Red</label>
+          <input type="radio" name="pokemon" value="Blue" id="pokemonBlue"><label for="pokemonBlue">Blue</label>
+        </div>
+        <div class="large-6 columns">
+          <label>Check these out</label>
+          <input id="checkbox1" type="checkbox"><label for="checkbox1">Checkbox 1</label>
+          <input id="checkbox2" type="checkbox"><label for="checkbox2">Checkbox 2</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="large-12 columns">
+          <label>Textarea Label
+            <textarea placeholder="small-12.columns"></textarea>
+          </label>
+        </div>
+      </div>
+    </form>
+	</div>
+</div>
+</body>
